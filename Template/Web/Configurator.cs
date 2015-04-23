9b0463f96e8eb6f23dce2f051114463920ed5d9c
Einar Ingebrigtsen <einar@dolittle.com>
2015-04-23 18:07:41 +0200

    Initial Commit

---
+++ Template/Web/Configurator.cs
@@ -0,0 +1,30 @@
+﻿using Bifrost.Configuration;
+
+namespace Web
+{
+    public class BifrostConfigurator : ICanConfigure
+    {
+        public void Configure(IConfigure configure)
+        {
+            configure
+                .Serialization
+                    .UsingJson()
+                .Frontend
+                    .Web(w =>
+                    {
+                        w.AsSinglePageApplication();
+
+                        var baseNamespace = global::Bifrost.Configuration.Configure.Instance.EntryAssembly.GetName().Name;
+                        var @namespace = string.Format("{0}.**.", baseNamespace);
+
+                        w.PathsToNamespaces.Add("**/", @namespace);
+                        w.PathsToNamespaces.Add("/**/", @namespace);
+                        w.PathsToNamespaces.Add("", baseNamespace);
+
+                        w.NamespaceMapper.Add(string.Format("{0}.**.", baseNamespace), string.Format("{0}.Domain.**.", baseNamespace));
+                        w.NamespaceMapper.Add(string.Format("{0}.**.", baseNamespace), string.Format("{0}.Read.**.", baseNamespace));
+                        w.NamespaceMapper.Add(string.Format("{0}.**.", baseNamespace), string.Format("{0}.**.", baseNamespace));
+                    });
+        }
+    }
+}
