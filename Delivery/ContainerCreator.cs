b858d855299a4df2d78a92d72dde6a5e67007a1a
Einar Ingebrigtsen <einar@dolittle.com>
2015-04-28 22:10:27 +0200

    Delivery up and running

---
+++ Delivery/ContainerCreator.cs
@@ -0,0 +1,24 @@
+﻿using Bifrost.Configuration;
+using Bifrost.Execution;
+using Bifrost.Ninject;
+using Ninject;
+
+namespace Web
+{
+    public class ContainerCreator : ICanCreateContainer
+    {
+        public readonly static IKernel Kernel;
+
+        static ContainerCreator()
+        {
+            Kernel = new StandardKernel();
+        }
+
+        public IContainer CreateContainer()
+        {
+            
+            var container = new Container(Kernel);
+            return container;
+        }
+    }
+}
---
+++ Delivery/TestBench/MessageBrokerHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json.Linq;
+using Web.Messaging;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Web.TestBench
+{
+    public class MessageBrokerHub : Hub
+    {
+        IMessageBroker _messageBroker;
+        JsonSerializer _serializer;
+
+        public MessageBrokerHub(IMessageBroker messageBroker)
+        {
+            _messageBroker = messageBroker;
+
+            _serializer = new JsonSerializer();
+            _serializer.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        public void Send(string messageAsString)
+        {
+            var messageHash = JObject.Parse(messageAsString);
+            
+            var messageTypeAsString = messageHash["messageType"].Value<string>();
+            var messageType = _messageBroker.GetMessageTypes().SingleOrDefault(t => t.Name == messageTypeAsString);
+            if (messageType != null)
+            {
+                var message = messageHash.ToObject(messageType, _serializer) as Message;
+                _messageBroker.Send(message);
+            }
+        }
+    }
+}
