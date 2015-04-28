683b47a03e8fd25c09c5c90a16a274ef50cb4d5b
Einar Ingebrigtsen <einar@dolittle.com>
2015-04-28 22:00:18 +0200

    Adding search

---
+++ Search/Messages/MaterialAdded.cs
@@ -0,0 +1,14 @@
+﻿using Web.Messaging;
+
+namespace Web.Messages
+{
+    public class MaterialAdded : Message
+    {
+        public string MaterialNumber { get; set; }
+        public string Vendor { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set;}
+    }
+}
---
+++ Search/Messaging/Message.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Web.Messaging
+{
+    public abstract class Message
+    {
+        public DateTime TimeStamp { get; set; }
+        public string MessageType { get; set; }
+    }
+}
