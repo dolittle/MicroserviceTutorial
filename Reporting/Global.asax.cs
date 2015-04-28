ef12cf608bcfcc9a9222aab15a9c934131d5fa06
Einar Ingebrigtsen <einar@dolittle.com>
2015-04-28 22:07:16 +0200

    Reporting up and running

---
+++ Reporting/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+using Bifrost.Extensions;
+
+namespace Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(RegisterWebApiRoutes);
+            GlobalConfiguration.Configuration.DependencyResolver = new Ninject.Web.WebApi.NinjectDependencyResolver(ContainerCreator.Kernel);
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+
+        void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        void RegisterWebApiRoutes(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
