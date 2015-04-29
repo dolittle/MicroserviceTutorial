0783418055e8298bde1d570c493c98bf608f9da4
jornhegstad <jorn.hegstad@gmail.com>
2015-04-29 13:54:23 +0200

    add missing file

---
+++ Catalog/Api/ProductDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Web.Models;
+
+namespace Web.Api
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public List<Product> GetByCategory(string category)
+        {
+            return Products.Where(x => x.Category.Equals(category)).ToList();
+        }
+
+        internal void Insert(Product product)
+        {
+            Products.Add(product);
+            SaveChanges();
+        }
+    }
+}
