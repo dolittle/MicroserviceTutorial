fe9dcdde780e70c60afe7a84664fc40d9a58c8d8
jornhegstad <jorn.hegstad@gmail.com>
2015-04-29 13:38:34 +0200

    Initial version

---
+++ Catalog/Api/CatalogApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Web.Messages;
+using Web.Messaging;
+using Web.Models;
+using Sample = Web.Messages.Sample;
+
+namespace Web.Api
+{
+    public class CatalogApiController : ApiController
+    {
+        IMessageBroker _messageBroker;
+
+        public CatalogApiController(IMessageBroker messageBroker)
+        {
+            _messageBroker = messageBroker;
+        }
+
+        [HttpGet]
+        public List<Product> GetProductsByCategory(string category)
+        {
+            var dbContext = new ProductDbContext();
+            return dbContext.GetByCategory(category);
+        }
+
+        [HttpGet]
+        public string AddToCart(string productId, decimal price)
+        {
+            var name = "Add to cart : "+productId;
+            _messageBroker.Send(new ItemAddedToCart
+            {
+                MaterialNumber = productId,
+                Price = price,
+                Quantity = 1
+            });
+
+            return "Added";
+        }
+    }
+}
---
+++ Catalog/CatalogBench/index.js
@@ -0,0 +1,35 @@
+﻿Bifrost.namespace("Web.TestBench", {
+    index: Bifrost.views.ViewModel.extend(function (messageTypes, messageBrokerHub) {
+        var self = this;
+        this.selectedMessageType = ko.observable({ properties: [] });
+        this.messageTypes = messageTypes.all();
+
+        this.values = {};
+
+        this.messages = ko.observableArray();
+
+        this.selectedMessageType.subscribe(function (messageType) {
+            if (Bifrost.isNullOrUndefined(messageType)) return;
+            self.values = {
+                messageType: messageType.name
+            };
+            messageType.properties.forEach(function (property) {
+                self.values[property] = ko.observable();
+            });
+        });
+
+        messageBrokerHub.client(function (client) {
+            client.received = function (message) {
+                var messageAsJson = ko.toJSON(message);
+                self.messages.push(messageAsJson);
+            };
+        });
+        
+        this.sendMessage = function () {
+            var values = ko.toJSON(self.values);
+            messageBrokerHub.server.send(values);
+        };
+
+
+    })
+});
